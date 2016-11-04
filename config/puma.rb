9ebf92e509548c3a3e701afc356c6e65f4fd3afd
Mario Goodwin <mr.goodwin@outlook.com>
2016-11-04 05:36:34 -0400

    fix production

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@ preload_app!
 
 rackup DefaultRackup
 port ENV['PORT'] || 3000
-enviorment ENV['RACK_ENV'] || 'development'
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
 	ActiveRecord::Base.establish_connection
